d142e8509acddccd0a4224af2faa90fc28ab7b96
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-02-19 23:04:44 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,96 @@
+// //var as a small lettr
+// let name="udya kumar"
+
+// console.log(name);
+// //var as a caps
+// let NAME="udya kumar"
+// console.log(NAME);
+// let name9="udya kumar"
+// console.log(name9);
+
+// let name_Pct="udya kumar"
+// console.log(name_Pct);
+// let n2ame="udya kumar"
+// console.log(n2ame);
+// let $name="udya kumar"
+// console.log($name);
+//  let a="uday kumar"
+//  let init1="G"
+//  let init2="k"
+//  console.log("my name is",a,init1,init2);
+
+//  //strings
+
+//  let name1="abhi"
+//  let name2="abhishek"
+//  let name3="abhibachan"
+//  let name4="abhikiran"
+//  let name5="abhijyothi"
+//  let name6="abhiking"
+//  let name7="abhiprincs"
+//  //console.log("hello my name is",name1);
+//  //console.log("hello my name is",name2);
+//  //console.log("hello my name is",name3);
+//  //console.log("hello my name is",name4);
+//  //console.log("hello my name is",name5);
+//  //console.log("hello my name is",name6);
+//  //console.log("hello my name is",name6);
+
+//  console.log("hello my name is",name1,"\nhello my name is",name2,"\nhello my name is",name3,"\nhello my name is",name4,"\nhello my name is",name5,"\nhello my name is",name6,"\nhello my name is",name7);
+ 
+// console.log("hello my name is",name1,"hello my name is",name2,"hello my name is",name3,"hello my name is",name4,"hello my name is",name5,"hello my name is",name6,"hello my name is",name7);
+// //console.log(`hello mt name is` &{name1} `hello i am `&{name2});
+// console.log(typeof(name1));
+ 
+
+// //number
+// let num=100
+// console.log(num);
+// console.log(typeof(num));//typeof() is used to check the data type of the value which is stored inside the variabe
+// console.log(Numberr);
+
+
+//num to str
+// let x=100
+// console.log(typeof(x));
+
+// console.log(x);
+// let  num=String(x)
+// console.log(num);
+// console.log(typeof(num));
+// //str to num
+// let y="100"
+// console.log(typeof(y));
+
+// console.log(y);
+// let  str=Number(x)
+// console.log(str);
+// console.log(typeof(str));
+
+
+// // str as a num converting
+// let name="uday"
+// console.log(name);
+// console.log(typeof(name));
+// let str_to_num=Number(name)
+// console.log(str_to_num);
+// console.log(typeof(str_to_num));
+// console.log(typeof(typeof(str_to_num)));//type of type of num var is str1
+
+// for  null here the null is consider as a object
+let a=null;
+console.log(a);
+console.log(typeof(a));
+
+a="Uday kumar"
+console.log(a);
+console.log(typeof(a));
+
+
+//converting num with str to the num
+let name_with_num="1000uday"
+console.log(name_with_num);
+console.log(typeof(name_with_num));
+let str1_to_num=Number(name_with_num)
+console.log(str1_to_num);
+console.log(typeof(str1_to_num));
