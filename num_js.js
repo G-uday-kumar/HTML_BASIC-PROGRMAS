7abb47d3c45c27ca899e795bb89e00facc974a4b
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-02-24 10:15:52 +0530

    Add files via upload

---
+++ num_js.js
@@ -0,0 +1,182 @@
+// date methods
+// let date=new Date();
+// console.log(date);
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getHours()); 
+// console.log(date.getMilliseconds());
+// console.log(date.getMinutes());
+// console.log(date.getMonth());
+// console.log(date.getSeconds());
+// console.log(date.toDateString());
+
+//arithametic operation
+// let a=100;
+// let b=200;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// let a="100";
+// let b=200;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// let a=true;
+// let b=200;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// let a="100";
+// let b="200";
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// let a=true;
+// let b=true;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// let a=100;
+// let b=true;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// let a=true;
+// let b="true";
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// let a=null;
+// let b=null;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+// let a=undefined;
+// let b=undefined;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// let a=null;
+// let b=undefined;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// let a=undefined;
+// let b=null;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+//incremental
+
+// let a=100;
+// console.log(a++);
+// console.log(a++);
+// console.log(++a);
+// console.log(a++);
+// console.log(++a);
+
+// //decremental
+// console.log(a--);
+// console.log(a--);
+// console.log(--a);
+// console.log(a--);
+// console.log(--a);
+
+//assigning operator
+// let a=10;
+// let b=5;
+// console.log(a+=b);//15
+// console.log(a-=b);//10
+// console.log(a*=b);//50
+// console.log(a/=b);//10
+// // console.log(a%=b);//0
+// console.log(a**=b);//0
+
+
+//comparision operator output is in the form of true or false
+
+// let a=10;
+// let b=5;
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a!=b);
+// console.log(a==b);
+// console.log(a===b);//it not only checking the  value it also checking the data type so it will result true
+
+
+
+// let a="10";
+// let b=10;
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a!=b);
+// console.log(a==b);//true bcz it check the value and the value is converted in the number sos it result it as a true
+// console.log(a===b);//it not only checking the  value it also checking the data type so it will result false
+
+
+// let a="10";
+// let b="10";
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a!=b);
+// console.log(a==b);
+// console.log(a===b);//it not only checking the  value it also checking the data type so it will result true
