2c1eed8b73b4a66867c697a4fe10c0bb99b673ef
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-02-25 23:09:01 +0530

    Add files via upload

---
+++ day25.js
@@ -0,0 +1,24 @@
+// let nam="uday";
+// console.log(nam);
+// let array=[1,2,3,5,"uday","kumar",true,false,Symbol,BigInt(100),null,undefined,[1,3,5,6],{nam:"uday kumar"}];
+// console.log(array);
+// console.log(array[2]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[8]);
+// console.log(array[13]);
+
+// let names=["uday","kumar","smrithi","mandana"];
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// console.log(names[5]);
+
+// console.log(`hello may name is ${names[0]}_${names[1]}`,`\nhello ${names[2]}_${names[3]} is my fav cricketor`);
+
+
+let movies=["kgf","kgf1","kaatera", "dhurandar","hit3","bahubali-1","kantara-1","pushpa-2"]
+let heros=["yash","darshan","ranveer","naani","prabhas","rishab","allu arjun"]
+let heroins=["srinidhi","ananya","sara","anushka","thamanna","rukku","rashmika"]
+let year=[2015,2016,2017,2018,2019,2020,2021,2022,2023]
+console.log(`\n movie name is ${movies[0]}_Hero is ${heros[0]}_heroin name is_${heroins[0]}_released in the year of_${year[0]}\n`,`movie name is ${movies[1]}_Hero is ${heros[0]}_heroin name is_${heroins[0]}_released in the year of_${year[1]}\n`,`movie name is ${movies[2]}_Hero is ${heros[1]}_heroin name is_${heroins[1]}_released in the year of_${year[2]}\n`,`movie name is ${movies[3]}_Hero is ${heros[2]}_heroin name is_${heroins[2]}_released in the year of_${year[3]}\n`,`movie name is ${movies[4]}_Hero is ${heros[3]}_heroin name is_${heroins[3]}_released in the year of_${year[4]}\n`,`movie name is ${movies[5]}_Hero is ${heros[4]}_heroin name is_${heroins[0]}_released in the year of_${year[5]}\n`,`movie name is ${movies[6]}_Hero is ${heros[5]}_heroin name is_${heroins[5]}_released in the year of_${year[6]}\n`,`movie name is ${movies[7]}_Hero is ${heros[6]}_heroin name is_${heroins[6]}_released in the year of_${year[7]}`);
