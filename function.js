2282997ebbd33a756065c7a8ac56ad83ae4a8497
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-03-03 23:29:45 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,51 @@
+// function wedding(){
+//     console.log(" Ravi weds Hamsa");
+
+// }
+// wedding();
+// wedding();
+// wedding();
+// wedding();
+
+
+//redule lines of code
+
+
+// let a=1;
+// let b=2;
+// let c=a+b;
+// console.log(c);
+// let a1=10;
+// let b1=20;
+// let c1=a1+b1;
+// console.log(c);
+// let a2=100;
+// let b2=200;
+// let c2=a2+b2;
+// console.log(c2);
+// let a3=1000;
+// let b3=2000;
+// let c3=a3+b3;
+// console.log(c3);
+// let a4=10000;
+// let b4=20000;
+// let c4=a4+b4;
+// console.log(c4);
+
+// now do that
+
+// function add(a,b){
+//     console.log(a+b);
+
+// }
+// add(1,2);
+// add(10,20);
+// add(100,200);
+// add(1000,2000);
+
+let a=["uday","kumar","hello"];
+let b=[1,2,3,4,5,6,7,8]
+console.log([...a,...b]);
+console.log(...a,...b);
+console.log(...a,b);
+console.log(a,...b);
