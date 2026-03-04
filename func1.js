857f8737d96efac70d3d9df0f69c6ab860e141e3
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-03-04 19:59:29 +0530

    Add files via upload

---
+++ func1.js
@@ -0,0 +1,180 @@
+// function greet(){
+//     console.log("Hello all good morning this is javascript class");
+//     console.log("how are you all");
+//     console.log("happy ugadi in advance");
+        
+// }
+// greet();
+
+// //finction with deefoult parameter
+// function add(a=10,b=20){
+//     let c=a+b;
+//     console.log("result opf addition is:-",c);
+// }
+// add()
+// //function iwth parameter with input from user
+// function sub(a=prompt("enter first no. :"),b=prompt("enter the seconf no. :")){
+//     let c=a-b;
+//     console.log("result of sub is:-",c);
+// }
+// // sub()
+
+// //funtion with  local scope variable
+// function add(){
+//     let a=100;
+//     let b=200;
+//     let c=a+b;
+//     console.log("addition result is :-",c);
+
+// }
+// add()
+// function sub(){
+//     let a=100;
+//     let b=200;
+//     let c=a-b;
+//     console.log("result after sub is:-",c);
+// }
+// sub()
+
+// //str methods in the fucntion
+// function concate(a="uday ",b="kumar"){
+//     let c=a.concat(b)
+//     console.log(c);
+// }
+// concate();
+
+// function char_at(a="Udya"){
+//     let b=a.charAt(2);//y
+//     console.log(b);
+// }
+// char_at()
+
+// function endswith(a="uday-kumar"){
+//     let b=a.endsWith("kumar");//true
+//     console.log(b);
+// }
+// endswith()
+
+// function incldes(a="Uday kumar"){
+//     let b=a.includes("day");
+//     console.log(b);
+// }
+// incldes()
+
+// function indexof(a="raj_kumar"){
+//     let b=a.indexOf("k");
+//     console.log(b);
+// }
+// indexof();
+
+// function lastindex_of(a="Kiccha_sudeep"){
+//     let b=a.lastIndexOf("u");
+//     console.log(b);
+// }lastindex_of()
+
+// function pad_end(a="kumarram"){
+//     let b=a.padEnd(17,"Ram");
+//     console.log((b));
+// }
+// pad_end()
+
+// function padstart(a="kiccha_sudeep"){
+//     let b=a.padStart(20,"baadsha_ ")
+//     console.log(b);
+// }padstart()
+
+// function repeatt(a="rahul"){
+//     console.log(a.repeat(5));
+// }repeatt()
+
+// function slicee(a="rockin star Yash"){
+//     console.log(a.slice(7,18));
+// }
+// slicee()
+
+// function substar(a="Kumar sangakar"){
+//     console.log(a.substring(4,8));
+// }substar()
+
+// function splitt(a="uday kumar"){
+//     console.log(a.split("uday",3));
+// }splitt()
+
+// function starts_with(a="uday kumar"){
+//     console.log(a.startsWith("uday"));
+
+
+// }starts_with()
+
+// function tolowercase(a="UDAY_Kumar"){
+//     console.log(a.toLocaleLowerCase());
+// }tolowercase()
+
+// function toupper(a="kiccha sudeep"){
+//     console.log(a.toUpperCase());
+// }toupper()
+
+
+// function to_str(a=100){
+//     console.log(a.toString());
+// }to_str()
+
+
+// //operators Do in the home
+
+// let x=10;
+// let y=20;
+// function 
+
+
+
+
+// //all array methods using the global variable
+// let a=["uday","vinay","ganesh","chiru","pradhyumna","manvitha"];
+// let b=[1,2,3,4,5,6,7,8,9,0];
+// function concate() {
+//     let c=a.concat(b)
+// }concate()
+
+// function floatt(){
+//     console.log(a.flat(b));
+
+// }floatt()
+
+// function includes(){
+//     console.log(a.includes("uday"));
+// }includes()
+
+// function indexof(){
+//     console.log(a.indexOf("vinay"));
+// }indexof()
+
+// function join(){
+//     console.log(a.join(b));
+
+// }join()
+
+// function last_ind(){
+//     console.log(a.lastIndexOf("chiru"));
+// }last_ind()
+
+
+//do reaminngs
+
+//functions with passing the parameter in the function call or a method
+
+function add(a,b){
+    console.log(a+b);
+}add(3,5)
+
+function concat(a,b){
+    console.log(a.concat(b));
+}concat("uday_","kumar")
+
+function charat(a){
+    console.log(a.charAt(2));
+}charat("uday");
+
+function endswith(a){
+    let b=a.endswith;
+}endswith("uday")
