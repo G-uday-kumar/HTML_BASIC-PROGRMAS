8f161fec86d92ac6ad98a1cff4d62baa7cb7d812
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-03-06 22:26:06 +0530

    Add files via upload

---
+++ conditon.js
@@ -0,0 +1,285 @@
+// for two conditions
+
+
+
+
+
+
+// if(true){
+//     console.log("done");
+// }
+// else{
+//     console.log("not done");
+// }
+
+// if(false){
+//     console.log("done");
+// }
+// else{
+//     console.log("not done");
+// }
+
+// if(!true){
+//     console.log("done");
+// }
+// else{
+//     console.log("not done");
+// }
+
+
+// if(NaN){
+//     console.log("done");
+// }
+// else{
+//     console.log("not done");
+// }
+
+// if(null){
+//     console.log('Done');
+// }
+// else{
+//     console.log("Not Done");
+// }
+
+//  if(undefined){
+//     console.log('Done');
+// }
+// else{
+//     console.log("Not Done");
+// }
+
+
+//  if(""){
+//     console.log('Done');
+// }
+// else{
+//     console.log("Not Done");
+// }
+
+
+
+//  if(" "){
+//     console.log('Done');
+// }
+// else{
+//     console.log("Not Done");
+// }
+
+//  if("uday"){
+//     console.log('Done');
+// }
+// else{
+//     console.log("Not Done");
+// }
+
+
+//  if(!false){
+//     console.log('Done');
+// }
+// else{
+//     console.log("Not Done");
+// }
+
+
+//  write a prog to check weether  the user is eligeble to vote or not 
+// let user=Number(prompt("enter the age"));
+// if(user>=18){
+//     console.log("eligible to vote");
+
+// }
+// else{
+//     console.log("not ");
+// }
+
+// // eligible to drive
+// let user=Number(prompt("enter the age"));
+// if(user>=18){
+//     console.log("eligible to drive");
+
+// }
+// else{
+//     console.log("not eligible to drive ");
+// }
+
+
+
+// // check no is positive or not
+// let user=Number(prompt("enter the number to check"));
+// if(user>0){
+//     console.log("the number is positive");
+
+// }
+// else{
+//     console.log("the number is negative ");
+// }
+
+
+
+// // find the biggest numbe ramong two  number
+// let num1=Number(prompt("enter the num1"));
+// let num2=Number(prompt("enter the num1"));
+// if(num1>num2){
+//     console.log("no_1 is greater");
+// }
+// else{
+//     console.log("no_2 is greater");
+// }
+
+
+
+
+// // check gengder
+// let gender=prompt("entr te gender").toLocaleLowerCase();
+// if(gender=="male"||gender=="m"){
+//     console.log("the gender is male");
+// }
+// else{
+//     console.log("female");
+// }
+
+
+//for the n=checking the input is numbe or not
+
+// let a=prompt("enter item")
+// if(a-1){
+//     console.log("yess its number.");
+// }
+// else{
+//     console.log("yes its a string");
+// }
+
+
+// let a=prompt("enter item")
+// if(Number(a)){
+//     console.log("yess its number.");
+// }
+// else{
+//     console.log("yes its a string");
+// }
+
+
+// let a=prompt("enter item")
+// if(!isNaN(a)){
+//     console.log("yess its number.");
+// }
+// else{
+//     console.log("yes its a string");
+// }
+
+
+// let a=Number(prompt("enter item"))
+// if(a-1){
+//     console.log("yess its number.");
+// }
+// else{
+//     console.log("yes its a string");
+// }
+
+// let a=prompt("enter item")
+// if(isNaN(a)){
+//     console.log("yess its str.");
+// }
+// else{
+//     console.log("yes its a num");
+// }
+
+// // checek no is zero ro not
+// let no=Number(prompt("enter the number"));
+// if(no===0){
+
+//     console.log("the number is zero");
+// }
+// else{
+//     console.log("the enterd one is non zero number");
+// }
+
+
+
+// //  for three conditon using the  if, else if, else.
+
+
+
+// // check given no is +ve or -ve or 0
+// let num=Number(prompt("enter the number"))
+// if (num>0){
+//     console.log("this is +ve");
+// }
+// else if(num===0){
+//     console.log("this is 0");
+// }
+// else if(num<0){
+//     console.log("this is -ve num");
+// }
+// else{
+//     console.log("enter the valid number");
+// }
+
+// //give  user is male  female or other gender
+// let gender=prompt("enter the gender").toLocaleLowerCase();
+// if (gender==="m"|| gender==="male"){
+//     console.log("you are a male");
+// }
+// else if(gender==="f"||gender==="female"){
+//     console.log("you are a female");
+// }
+// else if(gender==="o"||gender==="other")
+// {
+//     console.log("your gender is other");
+// }
+// else{
+//     console.log("please provide a proper in");
+// }
+
+
+
+// // if the user is eligible to drive or not exceed 
+
+// let age=prompt("enter the age")
+// if(age>120||age<0){
+//     console.log("please enter the proper age");
+// }
+// else if(age>=18){
+//     console.log("you are eligible to drive");
+// }
+// else if(age<18){
+//     console.log("still you need to wait for 18 complete");
+
+// }
+// else{
+//     console.log("please enter a valid input ");
+// }
+
+
+// // vote
+// let age=prompt("enter the age")
+// if(age>120||age<0){
+//     console.log("please enter the proper age");
+// }
+// else if(age>=18){
+//     console.log("you are eligible to vote");
+// }
+// else if(age<18){
+//     console.log("still you need to wait for 18 complete");
+
+// }
+// else{
+//     console.log("please enter a valid input ");
+// }
+
+// bank application
+let usr=prompt("select option  BALANCE,DEPOSIT,WITHDRAW").toLowerCase()
+if(usr==="balance"||usr==="b"){
+    console.log("you selected for a balance so enter the amount:-");
+    
+    ("your total balance is"))
+}
+else if(usr==="deposit"||usr==="d"){
+    console.log("you selected for the deposition");
+}
+else if(usr==="withdraw"||usr==="w"){
+    console.log("you selected for withdrawal");
+}
+else{
+    console.log("please select a proper one");
+}
+
