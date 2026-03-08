b8a9d394004a8e05c97465f006225b1d9824af78
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-03-08 10:12:19 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,80 @@
+// prog to check the numb is div by 3
+
+// let num=Number(prompt("enetr the nu"));
+// if(isNaN(num)){
+//     alert("please give a proper input")
+// }
+// else if(num%3===0){
+//     console.log("yes the number is div by 3");
+// }
+// else{
+//     console.log("not div by 3");
+// }
+
+
+// // prog to check the num div by 5
+// let num=Number(prompt("enetr the nu"));
+// if(isNaN(num)){
+//     alert("please give a proper input")
+// }
+// else if(num%5===0){
+//     console.log("yes the number is div by 5");
+// }
+// else{
+//     console.log("not div by 5");
+// }
+
+
+
+// prog to num div by both
+
+// let num=Number(prompt("enetr the nu"));
+// if(isNaN(num)){
+//     alert("please give a proper input")
+// }
+// else if(num%3===0 && num%5===0){
+//     console.log("yes the number is div by 3 &5");
+// }
+// else{
+//     console.log("not div by 3&5");
+// }
+
+// check wether the currrent time is am or pm 
+
+// let ti=new Date()
+// let tim=ti.getHours()
+// console.log(tim);
+// // console.log(ti.);
+// if (tim<12){
+//     console.log("this is AM");
+// }
+// else{
+//     console.log("this is a PM");
+// }
+
+
+// prog to check which date today
+// which month today
+
+
+
+// prog to convert the Hours to AM PM timings
+
+
+
+// prog to check wether the student is pass or fail input of 5 marks
+
+// let sub1=Number(prompt("entert a sub1 marks"))
+// let sub2=Number(prompt("entert a sub2 marks"))
+// let sub3=Number(prompt("entert a sub3 marks"))
+// let sub4=Number(prompt("entert a sub4 marks"))
+// let sub5=Number(prompt("entert a sub5 marks"))
+// let result=((sub1+sub2+sub3+sub4+sub5)/500)*100;
+// if(sub1>100||sub1<0 ||sub2>100||sub2<0 ||sub3>100||sub3<0 ||sub4>100||sub4<0 ||sub5>100||sub5<0 ){
+//     alert("please enter a proper marks")
+// }
+// else if(sub1>25 && sub2>35 && sub3>35 && sub4>35 && sub5>5&&result>34){
+//     console.log("you are pass");
+// } else{
+//     console.log("you are fail");
+// }
