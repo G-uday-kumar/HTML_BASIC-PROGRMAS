b5ff1276ec2f498e78db635d769f82c15ac1eb9a
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-04-01 06:22:42 +0530

    Add files via upload

---
+++ Dom1.js
@@ -0,0 +1,82 @@
+// let fruits=document.getElementsByClassName("fruits")
+// let Mango=document.getElementsByClassName("fruits")[1]
+// console.log(fruits);
+// console.log(Mango.innerText);
+// Mango.style.color="orange"
+
+let main=document.getElementsByClassName("main")[0]
+main.style.backgroundColor="grey"
+let header=document.getElementsByClassName("h")[0]
+header.style.color="white"
+
+
+
+let header1=document.getElementsByClassName("h")[1]
+header1.style.textTransform = "capitalize"
+header1.style.backgroundColor="pink"
+header1.style.borderRadius="15px"
+header1.style.textAlign="center"
+header1.style.fontSize="35px"
+header1.style.padding="10px"
+
+
+let h3=document.getElementsByClassName("h")[2]
+h3.style.textTransform = "capitalize"
+// h3.style.backgroundColor="teal"
+h3.style.borderRadius="15px"
+h3.style.textAlign="left"
+h3.style.fontSize="28px"
+h3.style.padding="15px"
+
+let h2=document.getElementsByClassName("h")[0]
+h2.style.textTransform = "capitalize"
+// h2.style.backgroundColor="teal"
+h2.style.borderRadius="15px"
+h2.style.textAlign="left"
+h2.style.fontSize="28px"
+h2.style.padding="10px"
+
+let p=document.getElementsByClassName("p")[0]
+p.style.textTransform = "capitalize"
+// p.style.backgroundColor="pink"
+p.style.borderRadius="15px"
+p.style.textAlign="center"
+p.style.fontSize="20px"
+p.style.padding="10px"
+p.style.textAlign="justify"
+
+
+let a=document.getElementsByClassName("a")[0]
+a.style.textTransform = "capitalize"
+a.style.backgroundColor="teal"
+a.style.borderRadius="15px"
+a.style.textAlign="left"
+a.style.fontSize="28px"
+a.style.padding="15px"
+
+let div=document.getElementsByClassName("div")[0]
+div.style.display="flex"
+div.style.textTransform = "capitalize"
+div.style.backgroundColor="wheat"
+div.style.borderRadius="15px"
+div.style.textAlign="left"
+div.style.fontSize="28px"
+div.style.padding="15px"
+div.style.justifyContent="space-bitween"
+
+let btn=document.getElementsByClassName("btn")[0]
+btn.style.textTransform = "capitalize"
+btn.style.backgroundColor="wheat"
+btn.style.borderRadius="15px"
+btn.style.textAlign="left"
+btn.style.fontSize="28px"
+btn.style.padding="15px"
+
+let btn1=document.getElementsByClassName("btn")[1]
+btn1.style.textTransform = "capitalize"
+btn1.style.backgroundColor="wheat"
+btn1.style.borderRadius="15px"
+btn1.style.textAlign="left"
+btn1.style.fontSize="28px"
+btn1.style.padding="15px"
+btn1.style.textDecoration="none"
