c247553c4a6788cd0364001bf769bd92a116e0d4
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-04-02 22:46:28 +0530

    Add files via upload

---
+++ dom_manipulation.js
@@ -0,0 +1,75 @@
+// let btn=document.getElementsByTagName("button")[0]
+// console.log(btn);
+// // btn.innerText="Submit-Button"
+// btn.style.backgroundColor="green"
+// btn.style.borderRadius="10px"
+// btn.style.fontSize="30px"
+// btn.style.padding="10px 20px"
+// btn.style.color="white"
+// btn.addEventListener("click",()=>{
+//     console.log("hello this is a Hello button you clicked ");
+// })
+// let btn=document.getElementsByTagName("button")[0]
+// btn.addEventListener("click",()=>{
+//     console.log("you clicked on apple");
+// })
+// btn=document.getElementsByTagName("button")[1]
+// btn.addEventListener("click",()=>{
+//     console.log("you clocked on mango");
+// })
+// btn=document.getElementsByTagName("button")[2]
+// btn.addEventListener("click",()=>{
+//     console.log("you clocked on banana");
+// })
+// btn=document.getElementsByTagName("button")[3]
+// btn.addEventListener("click",()=>{
+//     console.log("you clocked on Graphs");
+// })
+// btn=document.getElementsByTagName("button")[4]
+// btn.addEventListener("click",()=>{
+//     console.log("you clocked on papaya");
+// })
+
+// // btn=document.getElementsByTagName("button")[5]
+// // btn.addEventListener("click",()=>{
+// //     alert("you clocked submit")
+// // })
+
+// let inp=document.getElementsByTagName("input")[0]
+// console.log(inp.placeholder);
+
+// btn=document.getElementsByTagName("button")[5]
+// btn.addEventListener("click",()=>{
+//     console.log(inp.placeholder);
+//     console.log(inp.value);
+// })
+
+
+
+let inp=document.getElementsByTagName("input")[0]
+
+let inp1=document.getElementsByTagName("input")[1]
+let inp2=document.getElementsByTagName("input")[2]
+
+let btn=document.getElementsByTagName("button")[0]
+
+
+let card=document.getElementsByClassName("card")[0]
+
+btn.addEventListener("click",()=>{
+    // console.log(inp.value);
+    // console.log(inp1.value);
+    // console.log(inp2.value);
+    // if(inp.value==="" || inp1.value===""|| inp2.value===""){
+    //     alert("you should enter a value all are required field")
+    // }
+    // else{
+    //     //   alert(` name ${inp.value.concat(inp1.value)}\n mail is ${inp2.value}\n submitted `)
+
+   
+    // }
+
+
+
+})
+
